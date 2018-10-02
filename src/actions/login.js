b41f51fb18031510a68fd20c03eea1ae3533f0e5
Said <mokhammiedsaid@gmail.com>
2018-10-03 01:49:19 +0300

    verification

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,47 +1,69 @@
 import API from 'axios'
 import setToken from '../_config/setToken'
 import jwt_decode from 'jwt-decode'
+import { push } from 'react-router-redux'
 import { GET_ERRORS, SET_CURRENT_USER } from './constants'
 
 export const registerDispatch = (user, history) => dispatch => {
-  console.log(user)
-  API.post('/users/register', user)
-    .then(res => history.push('/sign-in'))
-    .catch(err => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    })
+	console.log(user)
+	API.post('/users/register', user)
+		.then(res => history.push('/verify-email'))
+		.catch(err => {
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		})
+}
+
+export const verifyEmailDispatch = user => dispatch => {
+	console.log(user)
+	API.post('users/verify_email', user)
+		.then(res => {
+			console.log(res.data)
+			const { token } = res.data
+			localStorage.setItem('jwtToken', token)
+			setToken(token)
+			const decoded = jwt_decode(token)
+			dispatch(setCurrentUser(decoded))
+			dispatch(push('/dashboard'))
+		})
+		.catch(err => {
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		})
 }
 
 export const loginDispatch = user => dispatch => {
-  API.post('/users/login', user)
-    .then(res => {
-      const { token } = res.data
-      localStorage.setItem('jwtToken', token)
-      setToken(token)
-      const decoded = jwt_decode(token)
-      dispatch(setCurrentUser(decoded))
-    })
-    .catch(err => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    })
+	API.post('/users/login', user)
+		.then(res => {
+			console.log(res.data)
+			const { token } = res.data
+			localStorage.setItem('jwtToken', token)
+			setToken(token)
+			const decoded = jwt_decode(token)
+			dispatch(setCurrentUser(decoded))
+		})
+		.catch(err => {
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		})
 }
 
 export const logoutDispatch = history => dispatch => {
-  localStorage.removeItem('jwtToken')
-  setToken(false)
-  dispatch(setCurrentUser({}))
-  history.push('/sign-in')
+	localStorage.removeItem('jwtToken')
+	setToken(false)
+	dispatch(setCurrentUser({}))
+	history.push('/sign-in')
 }
 
 export const setCurrentUser = decoded => {
-  return {
-    type: SET_CURRENT_USER,
-    payload: decoded
-  }
+	return {
+		type: SET_CURRENT_USER,
+		payload: decoded
+	}
 }
--- src/views/EmailVerify/EmailVerify.jsx
+++ src/views/EmailVerify/EmailVerify.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router-dom'
 // @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles'
 import Grid from '@material-ui/core/Grid'
@@ -12,82 +13,149 @@ import CardHeader from 'components/Card/CardHeader.jsx'
 import CardBody from 'components/Card/CardBody.jsx'
 import CardFooter from 'components/Card/CardFooter.jsx'
 
-const styles = {
-  cardCategoryWhite: {
-    color: 'rgba(255,255,255,.62)',
-    margin: '0',
-    fontSize: '14px',
-    marginTop: '0',
-    marginBottom: '0'
-  },
-  cardTitleWhite: {
-    color: '#FFFFFF',
-    marginTop: '0px',
-    minHeight: 'auto',
-    fontWeight: '300',
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: '3px',
-    textDecoration: 'none'
-  }
+import { verifyEmailDispatch } from '../../actions/login'
+
+const styles = theme => ({
+	cardCategoryWhite: {
+		color: 'rgba(255,255,255,.62)',
+		margin: '0',
+		fontSize: '14px',
+		marginTop: '0',
+		marginBottom: '0'
+	},
+	cardTitleWhite: {
+		color: '#FFFFFF',
+		marginTop: '0px',
+		minHeight: 'auto',
+		fontWeight: '300',
+		fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+		marginBottom: '3px',
+		textDecoration: 'none'
+	}
+})
+
+class EmailVerify extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			email: '',
+			verify_code: ''
+		}
+
+		this.inputOnChange = this.inputOnChange.bind(this)
+		this.formSubmit = this.formSubmit.bind(this)
+	}
+
+	inputOnChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+	formSubmit = e => {
+		e.preventDefault()
+		const user = {
+			email: this.state.email,
+			verify_code: this.state.verify_code
+		}
+		this.props.verifyEmailDispatch(user, this.props.history)
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.auth.isLogged) {
+			this.props.history.push('/')
+		}
+		if (nextProps.errors) {
+			this.setState({
+				errors: nextProps.errors
+			})
+		}
+	}
+
+	componentDidMount() {
+		if (this.props.auth.isLogged) {
+			this.props.history.push('/')
+		}
+	}
+
+	render() {
+		const { classes } = this.props
+		const { email, verify_code } = this.state
+		return (
+			<div>
+				<Grid container>
+					<GridItem xs={12} sm={12} md={8}>
+						<Card>
+							<form onSubmit={this.formSubmit}>
+								<CardHeader color="primary">
+									<h4 className={classes.cardTitleWhite}>
+										Email verification to finish registration with Voter App
+									</h4>
+									<p className={classes.cardCategoryWhite}>
+										Please, confirm email address
+									</p>
+								</CardHeader>
+								<CardBody>
+									<Grid container>
+										<GridItem xs={12} sm={12} md={6}>
+											<input
+												type="text"
+												labelText="Email address"
+												id="email_address"
+												name="email"
+												value={email}
+												onChange={this.inputOnChange}
+												formControlProps={{
+													fullWidth: true
+												}}
+											/>
+										</GridItem>
+									</Grid>
+									<Grid container>
+										<GridItem xs={12} sm={12} md={6}>
+											<input
+												type="text"
+												labelText="Verification code"
+												id="verification_code"
+												name="verify_code"
+												value={verify_code}
+												onChange={this.inputOnChange}
+												formControlProps={{
+													fullWidth: true
+												}}
+												inputProps={{ type: 'text' }}
+											/>
+										</GridItem>
+									</Grid>
+								</CardBody>
+								<CardFooter>
+									<Grid container>
+										<GridItem xs={12} sm={12} md={12}>
+											<Button type="submit" color="primary">
+												verify email
+											</Button>
+										</GridItem>
+										<GridItem xs={12} sm={12} md={12}>
+											<Link color="secondary" to="/sign-in">
+												already have an account? sign-in
+											</Link>
+										</GridItem>
+									</Grid>
+								</CardFooter>
+							</form>
+						</Card>
+					</GridItem>
+				</Grid>
+			</div>
+		)
+	}
 }
 
-function EmailVerify(props) {
-  const { classes } = props
-  return (
-    <div>
-      <Grid container>
-        <GridItem xs={12} sm={12} md={8}>
-          <Card>
-            <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>
-                Email verification to finish registration with Voter App
-              </h4>
-              <p className={classes.cardCategoryWhite}>
-                Please, confirm email address
-              </p>
-            </CardHeader>
-            <CardBody>
-              <Grid container>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Email address"
-                    id="email_address"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </Grid>
-              <Grid container>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Verification code"
-                    id="verification_code"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{ type: 'text' }}
-                  />
-                </GridItem>
-              </Grid>
-            </CardBody>
-            <CardFooter>
-              <Grid container>
-                <GridItem xs={12} sm={12} md={12}>
-                  <Button color="primary">verify email</Button>
-                </GridItem>
-                <GridItem xs={12} sm={12} md={12}>
-                  <Link color="secondary" to="/sign-in">
-                    already have an account? sign-in
-                  </Link>
-                </GridItem>
-              </Grid>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      </Grid>
-    </div>
-  )
-}
+const mapStateToProps = state => ({
+	auth: state.auth
+})
 
-export default withStyles(styles)(EmailVerify)
+export default connect(
+	mapStateToProps,
+	{ verifyEmailDispatch }
+)(withRouter(withStyles(styles)(EmailVerify)))
